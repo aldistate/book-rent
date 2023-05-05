b5653cee5ffe41f35349f8939cf794fffc9df074
aldistate <aldistate@yahoo.com>
2023-05-05 21:20:28 +0700

    first commit

---
+++ database/migrations/2023_05_05_122241_create_book_category_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_category', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id')
+                    ->constrained('books', 'id')
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+            $table->foreignId('category_id')
+                    ->constrained('categories', 'id')
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_category');
+    }
+};
---
+++ database/migrations/2023_05_05_141458_create_rent_logs_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rent_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                    ->constrained('users', 'id')
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+            $table->foreignId('book_id')
+                    ->constrained('books', 'id')
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+            $table->date('rent_date');
+            $table->date('return_date');
+            $table->date('actual_return_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rent_logs');
+    }
+};
