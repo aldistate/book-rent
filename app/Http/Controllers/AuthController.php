9a98fb76b3434e02b60cbe2e6367e620525c9201
aldistate <aldistate@yahoo.com>
2023-05-08 20:42:38 +0700

    refactor login form

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,16 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function index()
+    public function login()
     {
-        return view('auth.index');
+        return view('auth.login');
     }
 
-    public function login(Request $request)
+    public function auth(Request $request)
     {
+        // validasi inputan
+        $validated = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
 
+        // cek apakah data valid
+        if (Auth::attempt($validated)) {
+            // cek apakah status user active
+            if (Auth::user()->status != 'active') {
+                return redirect('/login')->with('failed', 'Your account is not active yet, please contact admin!');
+            }
+
+            // cek apakah rolenya admin
+            if (Auth::user()->role_id == 1) {
+                return redirect('dashboard');
+            }
+
+            // jika rolenya client
+            return redirect('profile');
+        }
+
+        return redirect('/login')->with('failed', 'Username or Password Invalid!');
     }
 }
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.main')
+@section('title', 'Dashboard')
+
+@section('container')
+  <h1>ini halaman dashboard</h1>
+@endsection
